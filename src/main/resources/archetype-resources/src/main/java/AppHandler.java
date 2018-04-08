701a692cea5b050fe86fa89e33fabbac09461fe9
Michael Slate <michael@codeguy.org>
2018-04-08 18:06:52 +0000

    added error handler override

--- src/main/resources/archetype-resources/src/main/java/AppHandler.java
+++ src/main/resources/archetype-resources/src/main/java/AppHandler.java
@@ -2,6 +2,7 @@ package ${package};
 
 import com.compellingcode.cloud.lambda.mvc.exception.EndpointConflictException;
 import com.compellingcode.cloud.lambda.mvc.handler.StreamHandler;
+import com.compellingcode.cloud.lambda.mvc.view.LambdaResponse;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -27,6 +28,11 @@ public class AppHandler  extends StreamHandler {
 		} catch(Exception ex) {
 			throw new EndpointConflictException(ex);
 		}
+	}
+	
+	@Override
+	protected LambdaResponse errorHandler(int errorNumber) {
+		return super.errorHandler(errorNumber);
 	}
 
 }
